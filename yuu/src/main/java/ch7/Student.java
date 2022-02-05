d9cba9922b3334d03486c2f8a1c9c6581e9ef23b
dbsghk208 <dbsghk208@hanmail.net>
2022-02-05 23:01:50 +0900

    상속 부모

---
+++ yuu/src/main/java/ch7/Student.java
@@ -0,0 +1,13 @@
+package ch7;
+
+//자식클래스
+public class Student extends People{
+	public int studentNo;
+	
+	public Student(String name, String ssn, int studentNo) {
+		super(name,ssn); //부모생성자 호출
+		this.studentNo= studentNo;
+	}
+	
+	
+}
