35c4fa1a44c899d0a59eb419dddfcf3f18c42eda
dbsghk208 <dbsghk208@hanmail.net>
2023-05-03 09:31:53 +0900

    에러 주석

---
+++ yuu/src/main/java/hongongja2023/chap01/exam01/VariableInitializationExample.java
@@ -0,0 +1,19 @@
+package hongongja2023.chap01.exam01;
+
+public class VariableInitializationExample {
+
+	public static void main(String[] args) {
+		//변수 선언
+		int value;
+		
+		//변수 value 값을 읽고 10 을 더하는 산술 연산을 수행
+		//연산의 결과값을 변수 result 초기값으로 저장
+	//	int result = value + 10;
+		
+		//변수 result 값을 읽고 콘솔에 출력
+	//	System.out.println(result);  
+		//--- value 변수가 값저장이 되지 않아 없는값+10 이 된 result 변수는 값을 읽을수 없음 컴파일 에러 발생함
+		
+		
+	}
+}
