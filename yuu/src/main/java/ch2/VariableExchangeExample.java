e4f2aac9f444f166a265928d66fb156dfda90171
dbsghk208 <dbsghk208@hanmail.net>
2023-05-03 09:30:28 +0900

    변수사용

--- yuu/src/main/java/ch2/VariableExchangeExample.java
+++ yuu/src/main/java/ch2/VariableExchangeExample.java
@@ -1,5 +1,6 @@
 package ch2;
 
+//변수값 교환
 public class VariableExchangeExample {
 
 	public static void main(String[] args) {
@@ -8,9 +9,9 @@ public class VariableExchangeExample {
 		
 		System.out.println("x: " + x + ", y: "+ y);
 		
-		int temp = x;
-		x = y;
-		y = temp;
+		int temp = x; //3
+		x = y;		  //5
+		y = temp;	  //3	
 		System.out.println("x: "+ x + ", y: "+ y);
 		
 
