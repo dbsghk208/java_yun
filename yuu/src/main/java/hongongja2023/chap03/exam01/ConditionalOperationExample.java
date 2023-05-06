6fe03265c7609bd4a70deb70a65e83f0b0d2c7e7
dbsghk208 <dbsghk208@hanmail.net>
2023-05-06 19:40:20 +0900

    커밋

--- yuu/src/main/java/hongongja2023/chap03/exam01/ConditionalOperationExample.java
+++ yuu/src/main/java/hongongja2023/chap03/exam01/ConditionalOperationExample.java
@@ -2,7 +2,7 @@ package hongongja2023.chap03.exam01;
 
 public class ConditionalOperationExample {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {  
 		int score = 85;
 		char grade = (score > 90) ? 'A' : 'B';
 		System.out.println(score+"점은 "+ grade + "등급입니다");
